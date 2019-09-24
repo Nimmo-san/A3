9775f48b16074fcc9cbfe3439ecbaa7b11df8fc4
Natu Okubet <n.okubet@se19.qmul.ac.uk>
2019-09-24 13:50:32 +0100

    Third Commit

--- FirstOne/FakeMain.py
+++ FirstOne/FakeMain.py
@@ -1,5 +1,4 @@
 
-
 # f(x) = 1 - a/x^2
 # f'(x) = a/x^3
 # Newton-Raphson method
@@ -7,31 +6,30 @@
 
 a=4.0;
 
+## Original function
 def func(x):
     return 1 - (a * (1/x**2))
 
-
+## Derivative of OF
 def derivfunc(x):
-    return a * (1/x**3)
+    return 2 * a * (1/x**3)
 
 
-# Function to find the root
+## Function to find the root
+## Using the Newton-Raphson method
 def newtonraphson(x0):
     b = func(float(x0));
     c = derivfunc(float(x0));
-    print b
-    print c
-    print b/c;
-    print x0-(b/c);
-    while abs(b/c) >= 0.00001:
-        b = func(float(x0));
-        c = derivfunc(float(x0));
+    
+    while abs(b/c) >= 0.000001:
+        b = func(x0);
+        c = derivfunc(x0);
         h =  x0 - (b / c)
-        print ("Solution found %.4f" % h);
-        x = x0 - (b/c);
-        x=x0;
+        print ("Solution found %.6f" % h);
+        x0=h;
 
-
+## Starting root
 x0 = float((a+1)/2);
 newtonraphson(x0)
+
 
