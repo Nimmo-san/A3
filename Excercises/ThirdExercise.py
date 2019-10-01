8bcbdb7483d92a2fbb33d7a5faee9a67941c56a7
Natu Okubet <n.okubet@se19.qmul.ac.uk>
2019-10-02 00:19:54 +0100

    Third Exercise

--- Excercises/ThirdExercise.py
+++ Excercises/ThirdExercise.py
@@ -1,4 +1,3 @@
-
 import math
 
 # Pythagorean Triplet
@@ -17,79 +16,59 @@ import math
 # X = | a  c-b |
 # det X = c^2 - a^2 - b^2
 
+# Euclid's Method
+# Try as long as  m>n
+#     a = m^2-n^2
+#     b = 2mn
+#     c = m^2+c^2
+# Dickinson's method #
+# Try again later #
+
 def coprime(a, b):
     while b:
         # b != 0
         a, b = b, a % b
     return a == 1
+    
+    
+nums = []
+a=0
+b=0
+c=0
+s = 1000
+m=0
+k=0
+n=0
+d=0
+mlimit = int(math.sqrt(s/2))
+found = False
+for m in range(2, mlimit):
+    if (s / 2) % m == 0:
+        if m % 2 == 0:
+            k = m + 1
+        else:
+            k = m + 2
+
+        while k<2*m and k<=s/(2*m):
+            if (s/(2*m)) % k == 0 and coprime(k,m) == True:
+                d = s/2/(k*m)
+                n=k-m
+                a = d*(math.pow(m,2) - math.pow(n,2))
+                b=2*d*n*m
+                c = d*(math.pow(m,2)+math.pow(n,2))
+                nums.append(int(a))
+                nums.append(int(b))
+                nums.append(int(c))
+                found = True
+                break
+            k = k+2
+    if found:
+        break
 
 
-divNums = []
-pairsNum = []
-numCoprimePairs = []
-num = 18
-
-
-def pythagorean_triples(n):
-    a, b, c = 1, 3, 0
-    while c < n:
-        a_ = (a * b) + a
-        c = math.sqrt(a_**2 + b**2)
-        if c == int(c):
-            yield b, a_, int(c)
-        a += 1
-        b += 2
-
-
-# Dickson's Method
-
-def factorpairs(n):
-     for i in range(1, num+1):
-        if n % i == 0:
-             divNums.append(i)
-     # divNums.append(num)
-
-
-def main():
-    for i in range(len(divNums)-len(divNums)/2):
-        a = 0
-        b = 0
-        if num % divNums[i] == 0:
-            a = divNums[i]
-            b = num / a
-            pairsNum.append(a)
-            pairsNum.append(b)
-
-
-def checkcoprime():
-    for i in range(len(pairsNum)-1):
-        if coprime(pairsNum[i], pairsNum[i+1]) == True:
-            numCoprimePairs.append(pairsNum[i])
-            numCoprimePairs.append(pairsNum[i+1])
-
-
-def findtriples():
-    x=0
-    y=0
-    z=0
-
-    for i in range(len(numCoprimePairs)-1):
-        s = numCoprimePairs[i]
-        t = numCoprimePairs[i+1]
-        r = int(math.pow(2 * num, 0.5))
-        x = r + s
-        y = r + t
-        z = r + s + t
-        print(int(math.pow(x, 2)),  " + ", int(math.pow(y, 2)), " = ", int(math.pow(z, 2)))
-
-
-factorpairs(num)
-main()
-checkcoprime()
-findtriples()
-# print divNums
-# print pairsNum
-print numCoprimePairs
+print (nums)
+print (nums[0]*nums[1]*nums[2])
+    
 
 
 
