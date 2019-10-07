a629548dfed4b6e0d32e042a9371ead99ae7fb5c
ap19161 <n.okubet@se19.qmul.ac.uk>
2019-10-07 15:47:48 +0100

    Assessed

---
+++ A1.4/A1.4 Exercise.py
@@ -0,0 +1,7 @@
+
+import math
+
+s = float(input("Enter a float: "))
+c = float(input("Enter a float: "))
+print("Angle: ", math.atan(s/c))
+print("Hypotenuse: ", math.sqrt(math.pow(s, 2) + math.pow(c,2)))
