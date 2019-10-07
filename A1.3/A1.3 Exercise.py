28c91edea7407f1f84820a996e24ce577ee9837a
ap19161 <n.okubet@se19.qmul.ac.uk>
2019-10-07 15:39:54 +0100

    Assessed

---
+++ A1.3/A1.3 Exercise.py
@@ -0,0 +1,8 @@
+
+import math
+
+
+x = float(input("Enter a float number, x= "))
+y = float(input("Enter a float number, y= "))
+z = float(input("Enter a float number, z= "))
+print(math.log(x+(y/z), math.e))
