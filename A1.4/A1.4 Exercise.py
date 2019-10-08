1ed2fcf892b72832a596651eed1045c74660b89e
Natu Okubet <n.okubet@se19.qmul.ac.uk>
2019-10-08 10:15:23 +0100

    Update A1.4 Exercise.py

--- A1.4/A1.4 Exercise.py
+++ A1.4/A1.4 Exercise.py
@@ -1,7 +1,6 @@
-
 import math
 
-s = float(input("Enter a float: "))
-c = float(input("Enter a float: "))
-print("Angle: ", math.atan(s/c) * 180/math.pi)
-print("Hypotenuse: ", math.sqrt(math.pow(s, 2) + math.pow(c, 2)))
+s = float(input())
+c = float(input())
+print(math.atan(s/c) * 180/math.pi)
+print(math.sqrt(math.pow(s, 2) + math.pow(c, 2)))
