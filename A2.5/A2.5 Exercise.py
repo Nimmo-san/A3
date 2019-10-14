e834fef1837b2edc9a718177c60c6de37261b960
Natu Okubet <n.okubet@se19.qmul.ac.uk>
2019-10-14 23:40:40 +0100

    A2.5 Assessed

--- A2.5/A2.5 Exercise.py
+++ A2.5/A2.5 Exercise.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plot
 import math
 
 # Come back to this assessed work !!!
-
 list_p = []
+
+
 def polygon(xc , yc, nsides, length, angle):
 
     r = length * (1 / (2 * math.sin(math.pi / nsides)))
@@ -33,8 +34,6 @@ angle = float(input())
 
 list_p = polygon(xc, yc, nsides, length, angle)
 
-#print(list_p)
-
 f = open('G:\Documents\points.txt', 'w')
 f.write(str(len(list_p)))
 f.write("\n")
@@ -44,9 +43,6 @@ for i in range(len(list_p)):
         px = "{:5.6}".format(str(list_p[i][j]))
         py = "{:5.6}".format(str(list_p[i][j+1]))
         f.write(px + str(",") + py + "\n")
-        #print(px, py)
 f.close()
-
-
 
 
