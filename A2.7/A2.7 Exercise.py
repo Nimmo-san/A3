e19b82e2385cf97e68337ce0302100fb2191cba2
ap19161 <n.okubet@se19.qmul.ac.uk>
2019-10-15 11:41:43 +0100

    A2.7 Assessed

---
+++ A2.7/A2.7 Exercise.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plot
+import math
+
+
+def forward(state, distance):
+    print(state, distance)
+    # Returns if the state of the pen is up
+    if state[2] == False or state[2] == 0:
+        return state
+    # Moves in the direction its facing if state of pen is down
+    elif state[2] == True or state[2] == 1:
+        x1 = (int(distance) * math.cos(math.radians(state[len(state)-1])))
+        y1 = (int(distance) * math.sin(math.radians(state[len(state)-1])))
+        xlist = [state[0], x1]
+        ylist = [state[1], y1]
+        plot.plot(xlist, ylist, 'r')
+        return change_state(state, x1, y1)
+
+
+def rotate(state, angle):
+    print(state, angle)
+    if angle < 0:
+        print("Error!")
+        exit(1)
+    else:
+        list1 = list(state)
+        list1[len(list1)-1] = angle
+        state = tuple(list1)
+        return state
+
+
+def pen(state, value):
+    print(state, value)
+    if value == 'down':
+        list1 = list(state)
+        list1[2] = True
+        state = tuple(list1)
+        return state
+    elif value == 'up':
+        list1 = list(state)
+        list1[2] = False
+        state = tuple(list1)
+        return state
+    else:
+        print("Error! Pen")
+
+
+def change_state(state, x1, y1):
+    list1 = list(state)
+    list1[0] = x1
+    list1[1] = y1
+    return tuple(list1)
+
+
+def main():
+    filename = input()
+
+    axis = [-400, 400, -400, 400]
+    plot.axis('square')
+    plot.axis(axis)
+    plot.title('Name...')
+
+    # pen = (x, y, state, angle)
+    penstate = (0, 0, False, 45)
+    lines = []
+
+    f = open(filename, 'r')
+    lines = [line.rstrip('\n') for line in f]
+    # print(lines)
+    for i in range(len(lines)):
+        split = lines[i].split('<')
+        split2 = split[1].split('>')
+        command = split[0]
+        argument = split2[0]
+        print(command, argument)
+    plot.show()
+
+
+main()
