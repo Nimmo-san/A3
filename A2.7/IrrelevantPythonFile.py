aab08723d4d56024c36d38b570e22eec80edc140
ap19161 <n.okubet@se19.qmul.ac.uk>
2019-10-20 01:01:37 +0100

    A2.7 Assessed

--- A2.7/IrrelevantPythonFile.py
+++ A2.7/IrrelevantPythonFile.py
@@ -4,25 +4,30 @@
 # for the matter of debugging, it is not included
 # with the actual code for this project
 # It is for the development of the REPEAT function
-# def main():
-#     command = ''
-#     argument = ''
-#     filename = input()
-#     f = open(filename, 'r')
-#     lines = [line.rstrip('\n') for line in f]
-#
-#     for i in range(len(lines)):
-#         split = lines[i].split('<')
-#         if i != len(lines)-1:
-#             split2 = split[1].split('>')
-#             command = split[0]
-#             argument = split2[0]
-#         else:
-#             split = lines[i].split(' ')
-#             split2 = split[1].split(' ')
-#             command = split[0]
-#             argument = split2[0]
-#         print(command, argument)
-#
-#
-# main()
+
+list_repeat = []
+
+
+def main():
+    command = ''
+    argument = ''
+    filename = 'G:\Documents\Faketest.txt'
+    f = open(filename, 'r')
+    lines = [line.rstrip('\n') for line in f]
+
+    for i in range(len(lines)):
+        try:
+            split = lines[i].split('<')
+
+            if split[0] == 'REPEAT':
+                while not 'REPEAT':
+                    list_repeat.append(split[0], split[1])
+
+        except ValueError:
+            print("Cant split!")
+
+        print(command, argument)
+        print(list_repeat)
+
+
+main()
