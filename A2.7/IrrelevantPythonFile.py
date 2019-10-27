3b541b2d1ebea4ad17c6fe0a7949c9720a887fa8
ap19161 <n.okubet@se19.qmul.ac.uk>
2019-10-27 19:30:10 +0000

    A2.7 Assessed

--- A2.7/IrrelevantPythonFile.py
+++ A2.7/IrrelevantPythonFile.py
@@ -4,8 +4,8 @@
 # for the matter of debugging, it is not included
 # with the actual code for this project
 # It is for the development of the REPEAT function
-
-list_repeat = []
+#
+all_func = []
 repeatfunc = []
 
 def main():
@@ -13,6 +13,8 @@ def main():
     argument = ''
     split = []
     split2 = []
+    rep_list = []
+    found_repeat = 0
     wrong = False
     found = False
     state_pen = (0, 0, False, 0)
@@ -35,27 +37,56 @@ def main():
         argument = split2[0]
 
         if command == 'REPEAT' and argument != 'END':
+            found_repeat = i
             found = True
 
         if command == 'REPEAT' and argument == 'END':
             found = False
 
-        if found:
+        if found and command != 'REPEAT':
             repeatfunc.append(command)
             repeatfunc.append(argument)
 
-        print(command, argument)
+        if command != 'REPEAT' and argument != 'END':
+            all_func.append(command)
+            all_func.append(argument)
+
+        change_to_tuple(repeatfunc)
+        change_to_tuple(all_func)
+
+        # print(command, argument)
     print(repeatfunc)
+    print(all_func)
+    print(merge_lists(rep_list, all_func, found_repeat))
 
 
-def repeat(state, list):
-    # print("This is not working!")
-    if isinstance(int(list[1]), int):
-        for i in range(2, int(list[1])):
-            print("command, argument ", list[i], list[i+1])
-    else:
+def repeat(list1, argument):
+    repeatfunc = []
+    if len(list1) <= 0:
         return
-    return state
+    for i in range(int(argument) * len(list1)):
+        for j in range(len(list1)):
+            a = list1[j]
+            repeatfunc.append(a)
+    print(len(list1))
+    return repeatfunc
+
+
+def change_to_tuple(list1):
+    list_tuple = []
+    for i in range(1, int(1/2 * len(list1))+1):
+        a = (list1[2*(i-1)], list1[2*i - 1])
+        list_tuple.append(a)
+    return list_tuple
+
+
+def merge_lists(fromlist, tolist, index):
+    if len(fromlist) <= 0:
+        return
+    for i in range(index, len(tolist)):
+        for j in range(len(fromlist)-1, -1, -1):
+            tolist.insert(i, fromlist[j])
+    return tolist
 
 
 main()
