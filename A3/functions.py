275f5fea5ef15a39680964169a6b84c9cd709955
ap19161 <n.okubet@se19.qmul.ac.uk>
2019-11-12 11:40:53 +0000

    A3

--- A3/functions.py
+++ A3/functions.py
@@ -53,39 +53,56 @@ def plotList(name, list_tuples, color, xaxis, yaxis):
 
 def remove_pattern(data):
     removed_pattern = []
+    # Checks if the data is null
     if len(data) <= 0:
         print("Length of data is null!")
+        # Returns none if null
         return None
 
+    # If not finds a pattern and replaces it
+    # with the chosen character
     for line in data:
         try:
             string = re.sub(' +', ',', line)
         except:
             pass
         removed_pattern.append(string)
+    # Then return the data after
     return removed_pattern
 
 
 def split_date(data):
     altered_data = []
+    # Checks if data is null
     if len(data) <= 0:
         print("Length of data is null")
+        # Returns none if null
         return None
 
+    # Iterates through the data
     for line in data:
+        # Splitting each line of the data
         split = line.split(',')
         try:
+            # Accessing the data from the index 0 till 7
             date = line[0:7]
         except:
+            # if not pass it
             pass
 
+        # Splitting the data to get the year and month
         dates = date.split('/')
         month = int(dates[1]) * 0.083
         roundedmonth = "{:2.3}".format(month)
 
+        # Making a new string based on the recently new data
+        # which is the normalisations of the month
         newstring = str(int(dates[0])+float(roundedmonth))
+        # The old is replaced with the new string using a list comprehension
         split = [word.replace(date, newstring) for word in split]
+        # appended into the data
         altered_data.append(split)
+    # and returned
     return altered_data
 
 
@@ -93,6 +110,8 @@ def correctFile(input_file, output_file):
     removed_pattern = []
     data = []
     full_data = []
+
+    # Ignores an error, rather than crash if the files dont exist
     try:
         toreadfile = open(input_file, 'r')
         writefile = open(output_file, 'w')
@@ -102,16 +121,21 @@ def correctFile(input_file, output_file):
         writefile = open(output_file, 'w+')
         exit(1)
 
+    # Reads the data in the file
     lines = [line.rstrip('\n') for line in toreadfile]
+    # Asks the function to remove the pattern
     removed_pattern = remove_pattern(lines)
+    # Requests for the data to be split
     data = split_date(removed_pattern)
     # print(data)
-    
+
+    # Iterates through joining each one
     for line in data:
         string1 = ','.join(line)
         full_data.append(string1)
 
-    print(full_data)
+    # print(full_data)
+
     toreadfile.close()
     writefile.close()
 
